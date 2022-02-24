cfc34838f5635a95735af3075ddf9afba999bd50
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-23 17:59:57 -0700

    finish User schema

--- server/models/User/index.js
+++ server/models/User/index.js
@@ -1,27 +1,44 @@
 const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
+const { hash, compare } = require("bcrypt");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    packs: {
+      type: String,
+      enum: ["", ""],
+      trim: true,
+    },
+    saveFiles: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "SaveFile",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  packs: {
-    type: String,
-    enum: ["", ""],
-    trim: true,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+// compare incoming password to hashed password
+// virtual for retrieving number of user's save files
+// virtual for retrieving number of user's packs
+module.exports = User;
