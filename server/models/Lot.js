31daa2b2ed520be9fa2b810b9413eadb733491df
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-24 13:26:39 -0700

    finish lot model

--- server/models/Lot.js
+++ server/models/Lot.js
@@ -1,12 +1,27 @@
 const { Schema, model } = require("mongoose");
 
-const lotSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Lot name is required!"],
-    trim: true,
+const lotSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Lot name is required!"],
+      trim: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    building: {
+      type: Schema.Types.ObjectId,
+      ref: "Building",
+    },
   },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const Lot = model("Lot", lotSchema);
 
