828344bd8bfc5950a0e2c2c7ba54e5ad6b024240
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-23 19:26:15 -0700

    set up SaveFile schema

--- server/models/SaveFile.js
+++ server/models/SaveFile.js
@@ -1,10 +1,15 @@
 const { Schema, model } = require("mongoose");
 
 const saveFileSchema = new Schema({
-    name : {
-        type: String,
-        required: [true, "Save File must have a name!"],
-        trim: true,
-    },
+  name: {
+    type: String,
+    required: [true, "Save File must have a name!"],
+    trim: true,
+  },
+});
 
-})
+// create model from schema
+const SaveFile = model("SaveFile", saveFileSchema);
+
+// export
+module.exports = SaveFile;
